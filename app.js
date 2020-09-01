c7de4bfbf8d97c9fef843ea5abcc8edfeded7935
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-01 16:48:29 -0500

    added values and a little bit of the functions

---
+++ app.js
@@ -0,0 +1,16 @@
+let resume = {
+    Name: "david swatzell",
+    Career: "Musician/Bartender",
+    Description: "I Collect Vinyl and produce records",
+    Interests: ['music', 'records', 'technology', 'theme parks', 'traveling'],
+    pastPositions: ["bartender", "musician"],
+    skills: "music production"
+
+}
+function displayPosition () {
+
+};
+
+function displaySkill () {
+
+};
