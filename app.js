d145c72d0d8640fdb0f3289db7bf593112a2db9f
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-09-04 15:32:25 -0500

    finally finished by adding the final displaySkill function

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 let resume = {
     Name: "david swatzell",
     Career: "Musician/Bartender",
-    Description: "I Collect Vinyl and produce records",
-    Interests: ["* Music", "* Records", "* Technology", "* Theme parks", "* Traveling"],
+    description: "I Collect Vinyl and produce records",
+    interests: ["* Music", "* Records", "* Technology", "* Theme parks", "* Traveling"],
     pastPositions: ["bartender", "musician"],
     skills: "music production"
 
 };
-console.log(("Name:"), resume.Name.toUpperCase());
-console.log(("Career:"), resume.Career);
-console.log(("Description:"), resume.Description);
+
+console.log("Name:", resume.Name.toUpperCase());
+console.log("Career:", resume.Career);
+console.log("Description:", resume.Description);
 console.log("My Interests:");
-console.log(resume.Interests[0]);
-console.log(resume.Interests[1]);
-console.log(resume.Interests[2]);
-console.log(resume.Interests[3]);
-console.log(resume.Interests[4]);
+// console.log(resume.Interests[0]);
+// console.log(resume.Interests[1]);
+// console.log(resume.Interests[2]);
+// console.log(resume.Interests[3]);
+// console.log(resume.Interests[4]);
+
+for (let i = 0; i < resume.interests.length; i++) {
+    console.log(resume.interests[i])
+}
+
 
 function displayPosition(companyName, jobTitle, description) {
-    console.log(companyName);
-    console.log(jobTitle);
-    console.log(description);
+    console.log("* " + jobTitle + " at " + companyName + " - " + description);
 };
+displayPosition("WRAY", "Guitar", "Shredding")
 
-function displaySkill() {
+function displaySkill(skillName, isCool) {
+    if (isCool === true) {
+        console.log("* BAM: " + skillName)
 
+    } else { console.log("* " + skillName) }
 };
+displaySkill("guitar", true)
+displaySkill("english", false)
